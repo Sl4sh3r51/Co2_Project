70ddfcd427e7326db6d8b8dcfbd0c3ce76302a6c
Sl4sh3r51 <dani.swan51@gmail.com>
2025-10-29 15:38:00 +0100

    Change EmissionData Upgrade Modal Content for Datenschutz

--- src/components/DatenschutzModalContent.vue
+++ src/components/DatenschutzModalContent.vue
@@ -1,7 +1,14 @@
 <template>
   <div>
     <h6>Datenschutzerklärung</h6>
-    <p>Wir nehmen den Schutz Ihrer persönlichen Daten sehr ernst. Ihre Daten werden im Rahmen der gesetzlichen Vorschriften geschützt und nicht an Dritte weitergegeben.</p>
+    <div class="text-muted mb-3">Stand: 01.01.2025</div>
+    <p>Der Schutz Ihrer Daten ist uns ein wichtiges Anliegen. Wir verarbeiten personenbezogene Daten ausschließlich nach den geltenden gesetzlichen Bestimmungen und geben diese nicht an Dritte weiter.</p>
+    <br>
+    <p>Unsere Website verwendet ausschließlich fiktive CO₂-Daten von Ländern und Unternehmen. Es werden keine Daten von Nutzer:innen erhoben, gespeichert oder weitergegeben.</p>
+    <br>
+    <p>Technisch notwendige Daten, wie beispielsweise Zugriffszeiten, Browsertyp oder IP-Adresse, werden anonymisiert erfasst, um den Betrieb der Website sicherzustellen und statistische Auswertungen zur Nutzung der Website zu ermöglichen. Auch diese Daten werden nicht an Dritte weitergegeben.</p>
+    <br>
+    <p>Mit der Nutzung unserer Website erklären Sie sich mit der Erhebung und Verarbeitung der Daten in der oben beschriebenen Form einverstanden.</p>
   </div>
 </template>
 <script setup lang="ts">
